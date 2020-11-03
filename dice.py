890f2364ac9494159f37f8c3ad00c5821b39983f
sushemsu <sushemsu@gmail.com>
2020-11-03 09:21:52 -0700

    Update dice.py
    DO IT

--- dice.py
+++ dice.py
@@ -17,5 +17,11 @@ def throw_dice(dice, max_val):
         throw = (systemRandom.randint(1,max_val))
         numbers = str(throw)
         print ("your throw is: " + numbers)
+        PORT = 8000
+        Handler = http.server.SimpleHTTPRequestHandler
+        httpd = socketserver.TCPServer(("", PORT), Handler)
+        print("serving at port", PORT)
+        httpd.serve_forever()
+        
         print ("yeah you thought");
 main()    
